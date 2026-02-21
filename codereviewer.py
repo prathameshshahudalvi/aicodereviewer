beb54d1c33ea023eaa03c125acc06a0616988834
prathameshdalvi <prathameshshahudalvi@gmail.com>
2026-02-21 12:38:25 +0530

    update: remove method = "json_schema

--- codereviewer.py
+++ codereviewer.py
@@ -15,9 +15,9 @@ def main():
     diff = subprocess.check_output(["git","show"], text=True)
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     llm = ChatGroq(model_name = model_name, api_key=GROQ_API_KEY)
-    response = llm.with_structured_output(HTMLResponse,method="json_schema").invoke(f"""Review the following code
+    response = llm.with_structured_output(HTMLResponse).invoke(f"""Review the following code
     changes and provide feedback, according to these rules:{RuleBook.rule} \n\n Diff:{diff}""")
-    print(response)
+    print(response.html)
 
 if __name__ == "__main__":
     main()
