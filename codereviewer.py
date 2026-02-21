18ba87940659866cf147124b5970cd011f24baff
prathameshdalvi <prathameshshahudalvi@gmail.com>
2026-02-21 12:22:30 +0530

    update: add rule script and groq llm

--- codereviewer.py
+++ codereviewer.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from aicodereviewer!")
+import subprocess 
+from langchain_groq import ChatGroq
+from rule import RuleBook
+import os
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 
+model_name = "llama-3.3-70b-versatile"
+
+class HTMLResponse(BaseModel):
+    html: str = Field(description="Valid HTML content")
+
+def main():
+    load_dotenv()
+    diff = subprocess.check_output(["git","show"], text=True)
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    llm = ChatGroq(model_name = model_name)
+    response = llm.with_structured_output(HTMLResponse).invoke(f"""
+    Summarize the following diff according to these rules:{RuleBook.rule}
+    Diff:{diff}""")
+    print(response)
 
 if __name__ == "__main__":
     main()
---
+++ rule.py
@@ -0,0 +1,5 @@
+class RuleBook:
+    rule = """
+Rule 1: The conversation name should be proper. It should be short (4 to 10 words) and easy to understand.
+Rule 2: The code should not contain any logical errors.
+"""
